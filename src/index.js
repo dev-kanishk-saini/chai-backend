c8b69a75f5ebd57b9b2f32b753682c362b7dda93
dev-kanishk-saini <kanishk.saini.cseiot.2021@miet.ac.in>
2024-12-15 12:09:43 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ dotenv.config({path : './env'})
 
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR :",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(` Server is running on port ; $ {process.env.PORT}`);
+    })
+
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})

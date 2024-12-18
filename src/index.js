fd332bbec1ac542faa7e9ead40641d3ba895689e
dev-kanishk-saini <kanishk.saini.cseiot.2021@miet.ac.in>
2024-12-18 14:05:13 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //require('dotenv').config({path : './env'});
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from "./app.js"
 dotenv.config({path : './env'})
 
 
@@ -13,7 +13,7 @@ connectDB()
         throw error
     })
     app.listen(process.env.PORT || 8000,() => {
-        console.log(` Server is running on port ; $ {process.env.PORT}`);
+        console.log(`Server is running on port : ${process.env.PORT}`);
     })
 
 })
